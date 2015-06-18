2716e34afaec3d93f5b3ce95da98b1368ebe1519
Alex Drenea <adrenea@infusion.com>
2015-06-18 18:30:10 -0400

    - added mobile app key and URI

--- Bootcamp2015-AmazingRace/ApplicationConstants.cs
+++ Bootcamp2015-AmazingRace/ApplicationConstants.cs
@@ -11,7 +11,9 @@ namespace Bootcamp2015.AmazingRace
         public static string MapToken = "kCuNbSR2vTk52kkDXaylmQ";
         public static string MapAppId = "931ea402-227c-4723-b4a4-68e799a1f30d";
 
-        public static string MobileServicesUri = "";
-        public static string MobileServicesAppKey = "";
+        ////PROD
+        public static string MobileServicesUri = "https://infusionamazingrace.azure-mobile.net/";
+        public static string MobileServicesAppKey = "XlYlBNtwuFzFROAbFpblbBQEPmdPJg64";
+
     }
 }
