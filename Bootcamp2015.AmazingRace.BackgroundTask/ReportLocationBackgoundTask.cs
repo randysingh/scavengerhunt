6f5505870b8106acb24991bdbcbbce6d5ce77a46
unknown <akisiel@KN-AKISIEL-01.corp.infusiondev.com>
2015-06-19 09:45:26 -0400

    added backgroundtask

---
+++ Bootcamp2015.AmazingRace.BackgroundTask/ReportLocationBackgoundTask.cs
@@ -0,0 +1,12 @@
+﻿using Windows.ApplicationModel.Background;
+
+namespace Bootcamp2015.AmazingRace.BackgroundTask
+{
+    public class ReportLocationBackgoundTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
