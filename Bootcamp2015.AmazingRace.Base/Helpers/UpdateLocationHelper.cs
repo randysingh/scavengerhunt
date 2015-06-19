b07e05cc1647f22b4ef6e9c9a98bbb2c41927d75
unknown <akisiel@KN-AKISIEL-01.corp.infusiondev.com>
2015-06-19 10:49:25 -0400

    updatelocation background threat

---
+++ Bootcamp2015.AmazingRace.Base/Helpers/UpdateLocationHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Bootcamp2015.AmazingRace.Base.ServiceInterfaces;
+using Caliburn.Micro;
+
+namespace Bootcamp2015.AmazingRace.Base.Helpers
+{
+    public static class UpdateLocationHelper
+    {
+        private static Geolocator locator = new Geolocator() { ReportInterval = 1000 };
+        private static IDataService dataService;
+        private static ISettingsService settingsService;
+
+
+        static UpdateLocationHelper()
+        {
+            dataService = IoC.Get<IDataService>();
+            settingsService = IoC.Get<ISettingsService>();
+        }
+
+        public async static Task UpdateLocation()
+        {
+            var pos = await locator.GetGeopositionAsync();
+            UpdateMyLocation(pos);
+        }
+
+        private static void UpdateMyLocation(Geoposition p)
+        {
+            var pos = new Geopoint(p.Coordinate.Point.Position);
+            var raceId = settingsService.GetValueOrDefault<string>("raceId", string.Empty);
+            dataService.PostTeamLocation(raceId, pos.Position.Latitude, pos.Position.Longitude);
+        }
+    }
+}
