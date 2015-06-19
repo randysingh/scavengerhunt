5efa541afd418a051afaa92573715390820b7ab7
Eugene Cheung <ey.cheung@outlook.com>
2015-06-18 23:22:19 -0400

    Clue and map views

--- Bootcamp2015-AmazingRace/ViewModels/MapPageViewModel.cs
+++ Bootcamp2015-AmazingRace/ViewModels/MapPageViewModel.cs
@@ -4,10 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
 
 namespace Bootcamp2015.AmazingRace.ViewModels
 {
     public class MapPageViewModel : Screen
     {
+        private Geolocator _locator;
+
+        public MapPageViewModel()
+        {
+            _locator = new Geolocator() { ReportInterval = 1000 };
+        }
     }
 }
