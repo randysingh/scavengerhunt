325881e5cb4d47a38209a8870e778609499dd824
unknown <asils@TN-ASILS-01.corp.infusiondev.com>
2015-06-19 12:04:46 -0400

    adding map files

---
+++ Bootcamp2015-AmazingRace/ViewModels/MapPageViewModel.cs
@@ -0,0 +1,131 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Media;
+
+namespace Bootcamp2015.AmazingRace.ViewModels 
+{
+    class MapPageViewModel : Screen
+    {
+        private Geolocator _locator;
+        private Geopoint _mapcenter;
+        private Geopoint _cluelocation;
+        private CoreDispatcher _dispatcher;
+
+        public MapPageViewModel(double latitude, double longitude)
+        {
+            _locator = new Geolocator() { ReportInterval = 1000 };
+
+            Pins = new ObservableCollection<PinViewModel>();
+            
+            var geopos = new BasicGeoposition();
+            geopos.Latitude = latitude;
+            geopos.Longitude = longitude;
+            geopos.Altitude = 120.0;
+            _cluelocation = new Geopoint(geopos);
+        }
+
+        public ObservableCollection<PinViewModel> Pins { get; set; }
+
+        public async Task Refresh()
+        {
+            var pos = await _locator.GetGeopositionAsync();
+            UpdateMyLocation(pos);
+            UpdateClueLocation();
+        }
+
+        public Geolocator Locator
+        {
+            get
+            {
+                return _locator;
+            }
+            set
+            {
+                _locator = value;
+            }
+        }
+
+        public Geopoint MapCenter
+        {
+            get
+            {
+                return _mapcenter;
+            }
+            set
+            {
+                _mapcenter = value;
+                NotifyOfPropertyChange(() => MapCenter);
+            }
+        }
+
+        public void UpdateClueLocation()
+        {
+            MapCenter = _cluelocation;
+            //RaisePropertyChanged("MapCenter");
+
+            var pin = new PinViewModel
+            {
+                Text = "clue location",
+                Color = new SolidColorBrush(Colors.Red),
+                Location = _cluelocation
+            };
+
+            if (Pins.Count > 1)
+            {
+                Pins.RemoveAt(1);
+            }
+            Pins.Insert(1, pin);
+        }
+
+        public void UpdateMyLocation(Geoposition p)
+        {
+            var pos = new Geopoint(p.Coordinate.Point.Position);
+            MapCenter = pos;
+            
+            //RaisePropertyChanged("MapCenter");
+
+            var pin = new PinViewModel
+            {
+                Text = "current location",
+                Color = new SolidColorBrush(Colors.Green),
+                Location = pos
+            };
+
+            if (Pins.Count > 0)
+            {
+                Pins.RemoveAt(0);
+            }
+            Pins.Insert(0, pin);
+        }
+
+        /*public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }*/
+
+        public class PinViewModel
+        {
+            public string Text { get; set; }
+            public Brush Color { get; set; }
+
+            public bool CanGetDirections { get; set; }
+            public Geopoint Location { get; set; }
+            public Point Anchor { get { return new Point() { X = 0.1, Y = 1 }; } }
+        }
+
+    }
+}
---
+++ Bootcamp2015-AmazingRace/Views/MapPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Bootcamp2015.AmazingRace.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace Bootcamp2015.AmazingRace.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MapPage : Page
+    {
+        MapPageViewModel mpvm;
+        public MapPage()
+        {
+            this.InitializeComponent();
+            mpvm = new MapPageViewModel(47.6, -122.3);
+            this.DataContext = mpvm;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            mpvm.Refresh();
+            mpvm.Locator.PositionChanged += OnLocatorPositionChanged;
+        }
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            mpvm.Locator.PositionChanged -= OnLocatorPositionChanged;
+        }
+
+        private async void OnLocatorPositionChanged(Geolocator sender, PositionChangedEventArgs args)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                mpvm.UpdateMyLocation(args.Position);
+            });
+        }
+
+
+    }
+}
