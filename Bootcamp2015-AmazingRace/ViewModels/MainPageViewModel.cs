874d1ffb5186e91f077c21d47c550157aaf1fd55
unknown <akisiel@KN-AKISIEL-01.corp.infusiondev.com>
2015-06-18 22:50:39 -0400

    marged conflict

--- Bootcamp2015-AmazingRace/ViewModels/MainPageViewModel.cs
+++ Bootcamp2015-AmazingRace/ViewModels/MainPageViewModel.cs
@@ -6,25 +6,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.ApplicationModel.Activation;
+using Bootcamp2015.AmazingRace.Base;
+using Bootcamp2015.AmazingRace.Helpers;
 
 namespace Bootcamp2015.AmazingRace.ViewModels
 {
-    public class MainPageViewModel : Screen
+    public class MainPageViewModel : Screen, IWebAuthenticationContinuable
     {
         private IMobileService mobileService;
-        
+
         public MainPageViewModel(ISettingsService settingsService, IMobileService mobileService)
         {
             this.mobileService = mobileService;
         }
 
+        private ICommand loginCommand;
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new DelegateCommand(() => this.Login());
+            }
+        }
+
         public async void Login()
         {
             this.mobileService.Initialize();
-            await this.mobileService.ServiceClient.LoginAsync(MobileServiceAuthenticationProvider.Google);
+            var result = await this.mobileService.ServiceClient.LoginAsync(MobileServiceAuthenticationProvider.Google);
 
             //client.CurrentUser 
         }
-        
+
+        public void ContinueWebAuthentication(WebAuthenticationBrokerContinuationEventArgs args)
+        {
+            if (args.Kind == ActivationKind.WebAuthenticationBrokerContinuation)
+            {
+                this.mobileService.ServiceClient.LoginComplete(args as WebAuthenticationBrokerContinuationEventArgs);
+            }
+
+        }
     }
 }
